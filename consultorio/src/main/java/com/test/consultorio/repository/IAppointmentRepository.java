211d843abe471e7b1f3910fbea3e381c3649ad93
Ramiro <ramiro.diaz@beexponentialgroup.com>
2025-05-09 16:51:48 -0600

    fix on repo

--- consultorio/src/main/java/com/test/consultorio/repository/IAppointmentRepository.java
+++ consultorio/src/main/java/com/test/consultorio/repository/IAppointmentRepository.java
@@ -9,7 +9,6 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.stereotype.Repository;
 
-import javax.print.Doc;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.List;
@@ -19,21 +18,24 @@ public interface IAppointmentRepository extends PagingAndSortingRepository<Appoi
         JpaRepository<Appointment, Integer>,
         JpaSpecificationExecutor<Appointment> {
 
-    @Query("SELECT a FROM Appointment a WHERE a.medicalOffice.id = :medicalOfficeId " +
+    @Query(value = "SELECT * FROM appointment a WHERE a.medical_office_id = :medicalOfficeId " +
             "AND a.enabled = true AND a.status NOT IN ('CANCELADA', 'COMPLETADA') " +
-            "AND (a.appointmentDate < :endTime AND :startTime < a.appointmentDate + INTERVAL '1 SECOND' * a.durationMinutes * 60)")
+            "AND (a.appointment_date < :endTime AND :startTime < a.appointment_date + INTERVAL 1 SECOND * a.duration * 60)",
+            nativeQuery = true)
     List<Appointment> findOverlappingByMedicalOffice(Integer medicalOfficeId, LocalDateTime startTime, LocalDateTime endTime);
 
-    @Query("SELECT a FROM Appointment a WHERE a.doctor.id = :doctorId " +
+    @Query(value = "SELECT * FROM appointment a WHERE a.doctor_id = :doctorId " +
             "AND a.enabled = true AND a.status NOT IN ('CANCELADA', 'COMPLETADA') " +
-            "AND (a.appointmentDate < :endTime AND :startTime < a.appointmentDate + INTERVAL '1 SECOND' * a.durationMinutes * 60)")
+            "AND (a.appointment_date < :endTime AND :startTime < a.appointment_date + INTERVAL 1 SECOND * a.duration * 60)",
+            nativeQuery = true)
     List<Appointment> findOverlappingByDoctor(Integer doctorId, LocalDateTime startTime, LocalDateTime endTime);
 
-    @Query("SELECT a FROM Appointment a WHERE a.patient.id = :patientId " +
+    @Query(value = "SELECT * FROM appointment a WHERE a.patient_id = :patientId " +
             "AND a.enabled = true AND a.status NOT IN ('CANCELADA', 'COMPLETADA') " +
-            "AND DATE(a.appointmentDate) = :date " +
-            "AND ((a.appointmentDate < :endTime AND :startTime < a.appointmentDate + INTERVAL '1 SECOND' * a.durationMinutes * 60) " +
-            "OR (a.appointmentDate + INTERVAL '1 SECOND' * a.durationMinutes * 60 + INTERVAL '2 HOURS' > :startTime))")
+            "AND DATE(a.appointment_date) = :date " +
+            "AND ((a.appointment_date < :endTime AND :startTime < a.appointment_date + INTERVAL 1 SECOND * a.duration * 60) " +
+            "OR (a.appointment_date + INTERVAL 1 SECOND * a.duration * 60 + INTERVAL 2 HOUR > :startTime))",
+            nativeQuery = true)
     List<Appointment> findOverlappingOrCloseByPatient(Integer patientId, LocalDateTime startTime, LocalDateTime endTime, LocalDate date);
 
     @Query("SELECT COUNT(a) FROM Appointment a WHERE a.doctor.id = :doctorId " +
@@ -51,7 +53,4 @@ public interface IAppointmentRepository extends PagingAndSortingRepository<Appoi
     List<Appointment> findByMedicalOfficeAndEnabledTrue(MedicalOffice medicalOffice);
 
     List<Appointment> findByDoctorAndEnabledTrue(Doctor doctor);
-    
 }
-
-
