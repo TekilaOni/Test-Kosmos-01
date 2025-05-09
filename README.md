56193696cc59d1b232ec6e839748fa6f3ea2129e
Ramiro <ramiro.diaz@beexponentialgroup.com>
2025-05-09 13:29:41 -0600

    add initial tables

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 # Test-Kosmos-01
-Cosmos Test
+Project created to make the kosmos test for job appliance
+
+# ALL YOU NEED
+```
+-Docker
+-Docker-compose (if you use Linux)
+-mvn
+-Java 17
+```
+# Instructions to execute
+```
+1.- In the main folder of the repo execute the DataBase Image
+
+    docker-compose up -d test-db
+
+2.- After this execute the db-migration project
+
+3.- After the db-migration project is executed (when is running)
+execute the main project "consultorio"
+
+```
--- db-migration/src/main/resources/db/migration/V202403121600__Initial_schema.sql
+++ db-migration/src/main/resources/db/migration/V202403121600__Initial_schema.sql
@@ -1,9 +1,34 @@
-CREATE TABLE employee (
+CREATE TABLE speciality (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(50) NOT NULL,
+    enabled BOOLEAN NOT NULL,
+    creator_user VARCHAR(30) NOT NULL,
+    creation_date TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    modification_date TIMESTAMP,
+    modification_user VARCHAR(30)
+);
+
+CREATE TABLE doctor (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(50) NOT NULL,
     last_name VARCHAR(100) NOT NULL,
     second_last_name VARCHAR(50),
-    employee_number VARCHAR(200) UNIQUE NOT NULL,
+    email VARCHAR(100) DEFAULT NULL,
+    phone_number VARCHAR(20) DEFAULT NULL,
+    speciality_id INT NOT NULL,
+    enabled BOOLEAN NOT NULL,
+    creator_user VARCHAR(30) NOT NULL,
+    creation_date TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    modification_date TIMESTAMP,
+    modification_user VARCHAR(30),
+    FOREIGN KEY (speciality_id) REFERENCES speciality(id)
+);
+
+CREATE TABLE patient (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(50) NOT NULL,
+    last_name VARCHAR(100) NOT NULL,
+    second_last_name VARCHAR(50),
     email VARCHAR(100) DEFAULT NULL,
     phone_number VARCHAR(20) DEFAULT NULL,
     enabled BOOLEAN NOT NULL,
@@ -13,17 +38,29 @@ CREATE TABLE employee (
     modification_user VARCHAR(30)
 );
 
-CREATE TABLE users (
+CREATE TABLE medical_office (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    username VARCHAR(36) NOT NULL,
-    password VARCHAR(100) NOT NULL,
-    employee_id INT NOT NULL,
-    user_role VARCHAR(20) NOT NULL,
+    number INT NOT NULL,
+    floor INT NOT NULL,
     enabled BOOLEAN NOT NULL,
-    register_timestamp TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    last_login_timestamp TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    failed_login_attempts INT NOT NULL DEFAULT 0,
-    lock_time TIMESTAMP NULL,
-    UNIQUE KEY username_uk(username),
-    CONSTRAINT fk_user_emp FOREIGN KEY (employee_id) REFERENCES employee(id) ON DELETE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+    creator_user VARCHAR(30) NOT NULL,
+    creation_date TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    modification_date TIMESTAMP,
+    modification_user VARCHAR(30)
+);
+
+CREATE TABLE appointment (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY,
+    doctor_id INT NOT NULL,
+    patient_id INT NOT NULL,
+    medical_office_id INT NOT NULL,
+    appointment_date TIMESTAMP NOT NULL,
+    enabled BOOLEAN NOT NULL,
+    creator_user VARCHAR(30) NOT NULL,
+    creation_date TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    modification_date TIMESTAMP,
+    modification_user VARCHAR(30),
+    FOREIGN KEY (doctor_id) REFERENCES doctor(id),
+    FOREIGN KEY (patient_id) REFERENCES patient(id),
+    FOREIGN KEY (medical_office_id) REFERENCES medical_office(id)
+);
